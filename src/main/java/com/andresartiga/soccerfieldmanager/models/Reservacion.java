fe56f65aa676ffa078ee92d0329a008bbca8007a
AndresArtiga333 <andresaartiga057@gmail.com>
2024-09-02 12:01:14 -0600

    feat: se modificaron las clases

--- src/main/java/com/andresartiga/soccerfieldmanager/models/Reservacion.java
+++ src/main/java/com/andresartiga/soccerfieldmanager/models/Reservacion.java
@@ -3,11 +3,14 @@ package com.andresartiga.soccerfieldmanager.models;
 import java.io.Serializable;
 import java.sql.Date;
 
+import com.andresartiga.soccerfieldmanager.utils.Status;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 @Entity
@@ -23,4 +26,11 @@ public class Reservacion implements Serializable {
     private Date endTime;
     private String payment;
 
+    private Status status;
+    @ManyToOne // defecto: fetch eager
+    // one to many y meny to many tiene  lazy
+    // primer palabra defina a la calse donde esta many es a reservaciones y la segunda hacia el atributo que esta direccionando, one es a user
+    private User user;
+    @ManyToOne
+    private SoccerField soccerField;
 }
---
+++ src/main/java/com/andresartiga/soccerfieldmanager/utils/Status.java
@@ -0,0 +1,7 @@
+package com.andresartiga.soccerfieldmanager.utils;
+
+public enum Status {
+    PROCESS,
+    RESERVED,
+    CANCELLED  
+}
